24933d3fe62504c004a7ecdec8fc655b7b2664ca
Afrose786 <afrosebanu200015@gmail.com>
2025-07-28 17:22:04 +0530

    Update monitor.cpp

--- monitor.cpp
+++ monitor.cpp
@@ -9,23 +9,23 @@ int vitalsOk(float temperature, float pulseRate, float spo2) {
 return vitalTemp(temperature)&&vitalPulseRate(pulseRate)&&vitalSpo2(spo2);
 }
 
-int vitalTemp(float temperature){
+int vitalTemp(float temperature) {
   if (temperature > 102 || temperature < 95) {
     cout << "Temperature is critical!\n";
     delay();
       return 0;
     }
     return 1;
-  } 
-  int vitalPulseRate(float pulseRate){
+  }
+  int vitalPulseRate(float pulseRate) {
   if (pulseRate < 60 || pulseRate > 100) {
     cout << "Pulse Rate is out of range!\n";
     delay();
     return 0;
     }
     return 1;
-  } 
-  int vitalSpo2(float spo2){
+  }
+  int vitalSpo2(float spo2) {
     if (spo2 < 90) {
     cout << "Oxygen Saturation out of range!\n";
     delay();
@@ -33,7 +33,7 @@ int vitalTemp(float temperature){
     }
   return 1;
   }
-void delay(){
+void delay() {
 for (int i = 0; i < 6; i++) {
       cout << "\r* " << flush;
       sleep_for(seconds(1));
